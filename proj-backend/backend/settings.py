bf647fef21726ade236b610719d5247c7f25b42c
Kileyydev <ivy.kiley@strathmore.edu>
2025-11-06 23:20:53 +0300

    fix: Change DEBUG setting to True for development environment

--- proj-backend/backend/settings.py
+++ proj-backend/backend/settings.py
@@ -20,7 +20,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # SECURITY & HOSTS
 # ===================================================================
 SECRET_KEY = env("SECRET_KEY", default="django-insecure-change-me-now")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
